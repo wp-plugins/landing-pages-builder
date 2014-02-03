3275217c9e7ab4baa3be37227cd8a4ffde6b9733
alitaj <alitaj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 00:05:38 +0000

    updated plugin description to try to drive more customer
    git-svn-id: https://plugins.svn.wordpress.org/landing-pages-builder/trunk@850208 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- landing-pages-builder.php
+++ landing-pages-builder.php
@@ -2,7 +2,7 @@
   /**
    * Plugin Name: Landing Pages Builder
    * Plugin URI: http://corp.wishpond.com/landing-page-builder/
-   * Description: Create amazing landing pages from your wordpress site. Monitor analytics and improve conversion rates, host your landing pages anywhere and more.
+   * Description: Create amazing landing pages from your wordpress admin and host them anywhere. Monitor analytics and improve conversion rates and much more.
    * Version: 1.1
    * Author: Wishpond
    * Text Domain: landing-pages-builder
